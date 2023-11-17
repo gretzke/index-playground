1e6038ecdbedefd68e7fcfdd88fcf908f86c95a3
gretzke <daniel@gretzke.de>
2023-11-17 23:18:02 +0300

    initial commit: tokens

--- index.py
+++ index.py
@@ -1,7 +1,26 @@
 from simulation import Simulation
 
 
+class Token():
+    symbol: str
+    amount: float
+    weight: float
+
+    def __init__(self, symbol: str, amount: float, weight: float = None):
+        self.symbol = symbol
+        self.amount = amount
+        self.weight = amount
+        if (weight != None):
+            self.weight = weight
+
+
 class Index(Simulation):
 
-    def __init__(self):
+    tokens: list[Token]
+
+    def __init__(self, tokens: list[Token], daysToSimulate: int = 0):
         super().__init__()
+        if (daysToSimulate > 0):
+            self.randomise(days=daysToSimulate)
+
+        self.tokens = tokens
